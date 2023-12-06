09fd30ea19757fa3eef69034701afb693416c67f
Skilled Cat <97121666+TheSkilledCat@users.noreply.github.com>
2023-12-06 15:25:33 +0330

    Added Options + Fixed Navbar Bugs

--- Home/assets/js/navbar.js
+++ Home/assets/js/navbar.js
@@ -1,25 +1,9 @@
-let nav_is_active = false;
-const nav = document.getElementById("navbar");
+const nav = document.querySelector(".navbar");
 const nav_icon = document.querySelector("#nav-toggle i");
 const nav_toggle = document.getElementById("nav-toggle");
 
-function open_nav() {
-    nav_is_active = true;
-    nav.style.display = "flex";
-    nav_toggle.classList.toggle("active");
-
-}
-
-function close_nav() {
-    nav_is_active = false;
-    nav.style.display = "none";
-    nav_toggle.classList.toggle("active");
-}
 
 function toggle_nav() {
-    if (nav_is_active) {
-        close_nav();
-    } else {
-        open_nav();
-    }
+    nav.classList.toggle("active")
+    nav_toggle.classList.toggle("active");
 }
