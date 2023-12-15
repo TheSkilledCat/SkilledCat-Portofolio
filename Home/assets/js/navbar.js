cca74ee71188071492233ea53f93a0eec56f297d
Skilled Cat <97121666+TheSkilledCat@users.noreply.github.com>
2023-12-15 16:54:14 +0330

    Updated navbar

--- Home/assets/js/navbar.js
+++ Home/assets/js/navbar.js
@@ -1,9 +1,11 @@
 const nav = document.querySelector(".navbar");
 const nav_icon = document.querySelector("#nav-toggle i");
 const nav_toggle = document.getElementById("nav-toggle");
+const hamburger = document.querySelector(".hamburger");
 
 
 function toggle_nav() {
     nav.classList.toggle("active")
     nav_toggle.classList.toggle("active");
+    hamburger.classList.toggle("is-active");
 }
